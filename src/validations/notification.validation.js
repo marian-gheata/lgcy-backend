542a2d749c24eb5805c757a4db8438c4d60b6afe
marian gheata <marian.gheata.dev@gmail.com>
2023-02-24 20:24:23 +0100

    complete notification and message

--- src/validations/notification.validation.js
+++ src/validations/notification.validation.js
@@ -9,11 +9,56 @@ const createNotification = Joi.object().keys({
     .keys({
       users: Joi.array().items(Joi.string().custom(objectId)),
       timelines: Joi.array().items(Joi.string().custom(objectId)),
-      posts: Joi.array().items(Joi.string().custom(objectId)),
+      notifications: Joi.array().items(Joi.string().custom(objectId)),
     })
     .min(1),
 });
 
+const getNotifications = {
+  query: Joi.object().keys({
+    title: Joi.string(),
+    description: Joi.string(),
+    role: Joi.string(),
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+  }),
+};
+
+const getNotification = {
+  params: Joi.object().keys({
+    notificationId: Joi.string().custom(objectId),
+  }),
+};
+
+const updateNotification = {
+  params: Joi.object().keys({
+    notificationId: Joi.required().custom(objectId),
+  }),
+  body: Joi.object().keys({
+    type: Joi.string(),
+    from: Joi.string().custom(objectId),
+    to: Joi.string().custom(objectId),
+    data: Joi.object()
+      .keys({
+        users: Joi.array().items(Joi.string().custom(objectId)),
+        timelines: Joi.array().items(Joi.string().custom(objectId)),
+        notifications: Joi.array().items(Joi.string().custom(objectId)),
+      })
+      .min(1),
+  }),
+};
+
+const deleteNotification = {
+  params: Joi.object().keys({
+    notificationId: Joi.string().custom(objectId),
+  }),
+};
+
 module.exports = {
   createNotification,
+  getNotifications,
+  getNotification,
+  updateNotification,
+  deleteNotification,
 };
